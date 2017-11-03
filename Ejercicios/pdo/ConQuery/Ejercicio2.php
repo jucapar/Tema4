81f75cd2567cc61aff9117a736563a997fa604ad
jucapar <juanpastorcarlos@gmail.com>
2017-11-03 21:31:19 +0100

    Añadidos ejercicios con query

---
+++ Ejercicios/pdo/ConQuery/Ejercicio2.php
@@ -0,0 +1,41 @@
+<?php
+
+    /*
+        Autor: Juan Carlos Pastor Regueras
+        Mostrar el contenido de la tabla Departamento y el número de registros.
+        Fecha de modificacion: 28-10-2017
+    */
+
+    //Información de la base de datos. Host y nombre de la BD
+	include "../config.php";
+   
+    try{
+        //Creamos la conexion a la base de datos
+        $db = new PDO($datosConexion,$user,$password);
+        //Definición de los atributos para lanzar una excepcion si se produce un error
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
+        //Creamos la consulta
+        $consulta = "SELECT * FROM Departamento";
+        //Ejecutamos la consulta
+        $resultado = $db->query($
+        //Guardamos el numero de registros obtenidos
+        $numRegistros = $resultado->rowCount();
+       
+		while ($departamento = $resultado>fetch(PDO::FETCH_OBJ)) {//Mientras haya resultados, se muestran formateados. FETCH avanza el puntero
+			echo "Codigo Departamento:".$departamento->CodDepartamento."<br />";
+			echo "Descripcion Departamento:".$departamento->DescDepartamento."<br />";
+			echo "<br />";
+		}
+    
+        //Cerramos la conexion
+        unset($db);
+       
+    }
+    catch(PDOException $PDOE){
+        //Capturamos la excepcion en caso de que se produzca un error,mostramos el mensaje de error y deshacemos la conexion
+        echo($PDOE->getMessage());
+        unset($db);
+        
+    }
+    
+?>
