2e762be6f1f56e3a11c0d75ffed7efe71ae96266
jucapar <juanpastorcarlos@gmail.com>
2017-11-06 12:19:25 +0100

    Ejercicios 6/11/2017

--- Ejercicios/pdo/ConQuery/Ejercicio8.php
+++ Ejercicios/pdo/ConQuery/Ejercicio8.php
@@ -1,19 +1,27 @@
 <?php
 /*
   Autor: Juan Carlos Pastor Regueras
-  Formulario para añadir un departamento a la tabla Departamento con validación de entrada y control de errores.
+  Página web que toma datos (código y descripción) de la tabla Departamento y guarda en un fichero departamento.xml. (COPIA DE SEGURIDAD / EXPORTAR)
   Fecha de modificacion: 28-10-2017
  */
 
-// Establecemos una nueva conexion
-include "../config.php";
-$db = new mysqli($host, $user, $password, $database);
 
 //Comprobamos si ha habido algun error de conexion, en tal caso mostramos el codigo de error
-if ($db->connect_errno) {
-    echo "Error al conectarse a la base de datos<br/>";
-    echo "Codigo de error:" . $db->connect_errno;
-} else {
+include "../config.php";
+
+try {
+    //Creamos la conexion a la base de datos
+    $db = new PDO($datosConexion, $user, $password);
+    //Definición de los atributos para lanzar una excepcion si se produce un error
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    //Si no se produce ningun error mostramos el mensaje de confirmacion
+    echo ("Conexion establecida");
+} catch (PDOException $PDOE) {
+    //Capturamos la excepcion en caso de que se produzca un error,mostramos el mensaje de error y deshacemos la conexion
+    echo($PDOE->getMessage());
+    unset($db);
+}
+
 
     if (!filter_has_var(INPUT_POST, 'Exportar')) {
         ?>
@@ -30,15 +38,16 @@ if ($db->connect_errno) {
         <?PHP
     } else {
         $consulta = "SELECT * from Departamento"; //Consulta de todos los registros para generar la tabla
-        $sentencia = $db->prepare($consulta); //Se prepara la consulta
-        $resultado = $db->execute(); //Se ejecuta la consulta
+        $resultado = $db->query($consulta); //Se almacena el resultado de la consulta
+        $registro = $resultado->fetch_object(); //Se obtiene   un  registro en el objeto (asociativo) y avanza el puntero por el conjunto de registros
         //Cotenido del fichero XML
         $xml = new SimpleXMLElement('<?xml version="1.0" encoding="UTF-8"?><Departamentos></Departamentos>'); //creación del XML y su nodo raiz
         header("Content-type: text/xml");
-         while ($departamento = $sentencia->fetch(PDO::FETCH_OBJ)) {//Mientras haya resultados, se imprimen. FETCH avanza el puntero
+        while ($registro != null) {//Mientras haya resultados, se imprimen. FETCH avanza el puntero
             $departamento = $xml->addChild('Departamento'); //nuevo elemento hijo
             $departamento->addChild('CodDepartamento', $registro->CodDepartamento); //nuevo elemento hijo de departamento
             $departamento->addChild('DescDepartamento', $registro->DescDepartamento); //nuevo elemento hijo de departamento
+            $registro = $resultado->fetch_object(); //Avanza el puntero al siguiente registro
         }
         print($xml->asXML()); //Se imprime el xml creado
     }
